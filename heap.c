6e21577ac84be63e7026e36bdb24bb3b6017e6cd
Replit user <>
2023-09-28 19:59:19 +0000

    new try

--- heap.c
+++ heap.c
@@ -33,6 +33,23 @@ void heap_pop(Heap* pq){
 }
 
 Heap* createHeap(){
+  Heap* reserva = (Heap*)malloc(sizeof(Heap));
 
-   return NULL;
+  if(reserva == NULL){
+    perror("Error al reservar memoria en el Heap");
+    exit(EXIT_FAILURE);
+  }
+
+  reserva->size = 0;
+  reserva->capac = 3;
+
+
+  reserva->heapArray = (heapElem*)malloc(sizeof(heapEle) * 3);
+
+  if(reserva->heapArray == NULL){
+    perror("Error al reservar la memoria en el arreglo heapArray");
+    exit(EXIT_FAILURE);
+  }
+
+  return reserva;
 }
