e929936ed46c9a3662a7aa51d630b2b40c6b3f76
Replit user <>
2023-10-10 20:23:48 +0000

    new try

--- heap.c
+++ heap.c
@@ -63,7 +63,7 @@ void heap_pop(Heap* pq){
     int rightChild = 2 * index + 2;
     int largest = index;
 
-    if(leftChild > pq->size && pq->heapArray[leftChild].priority < pq->heapArray[largest].priority){
+    if(leftChild < pq->size && pq->heapArray[leftChild].priority < pq->heapArray[largest].priority){
       largest = leftChild;
     }
 
